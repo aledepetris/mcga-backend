5c10ddf20d778d8b8077c7726194de3c46cfe79e
Alejandro Depetris <aledepetris@outlook.com>
2023-12-05 23:20:36 -0300

    Creacion de la interface Libro

---
+++ src/interfaces/libro.ts
@@ -0,0 +1,10 @@
+export interface Libro {
+    _id: string;
+    titulo: string;
+    autor: string;
+    isbn: string;
+    genero: string;
+    fechaPublicacion: Date;
+    editorial: string;
+    numPaginas: number;
+  }
