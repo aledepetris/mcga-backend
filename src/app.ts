e278f4af3bfdd947fa2ce5e1f0af3ce45ea98b41
Alejandro Depetris <aledepetris@outlook.com>
2023-12-05 22:34:59 -0300

    Actualización y mejora en tipados de app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { config } from 'dotenv';
+import router from './routes/router'
 
-const URL_DB = process.env.MONGO_URL;
-const PORT = process.env.PORT;
-
-const app = express();
+config();
+const URL_DB: string = process.env.MONGO_URL || '';
+const PORT: number = parseInt(process.env.PORT || '3000');
+const app: Express = express();
 
 app.use(express.json());
-app.use(cors({ origin: '*'}));
-
+app.use(cors({ origin: '*' }));
+app.use(router);
 
 mongoose.connect(URL_DB).then(() => {
   console.log("🟢 Base de datos conectada.");
-
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  })
+  });
 }).catch((err: any) => {
-  console.log('🔴 Hubo un error al intentar conectarse a la base de datos.');
+  console.log("🔴 Hubo un error al intentar conectarse a la base de datos.");
   console.log(err);
-})
+});
 
-module.exports = app;
+export default app;
